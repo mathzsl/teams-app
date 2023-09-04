8c3be7d9e7b1ec5cfff84fc60dd8939f3597fd8f
mathzsl <maths.soares99@gmail.com>
2023-09-04 17:12:56 -0300

    feat: listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
+
 import { Highlight } from "@components/Highlight";
-import { Container } from "./styles";
+import { Container, GroupList } from "./styles";
 import { Header } from "@components/Header";
 import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Os Cuecas",
+    "Gangue do Ovo",
+    'Gangue dos Bananas'
+  ]);
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" description="jogue com a sua turma" />
 
-      <GroupCard title="Os Cuecas" />
-      <GroupCard title="Os Cuecas" />
+      <GroupList
+        showsVerticalScrollIndicator={false}
+        data={groups}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => <GroupCard title={String(item)} />}
+      />
     </Container>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -5,3 +5,7 @@ export const Container = styled.View`
   padding: 42px 24px;
   background-color: ${({ theme }) => theme.COLORS.GRAY_700};
 `;
+
+export const GroupList = styled.FlatList`
+  flex: 1;
+`;
