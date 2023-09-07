663caf880cb34056843a7899e1fd155ee0b6b135
mathzsl <maths.soares99@gmail.com>
2023-09-07 05:34:27 -0300

    feat: preventing glitch in navigation

--- App.tsx
+++ App.tsx
@@ -20,7 +20,6 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="inverted" />
-
       {!fontsLoaded ? <Loading /> : <Routes />}
     </ThemeProvider>
   );
